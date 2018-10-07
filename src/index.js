95bde1942bb268f7eca9e17ae5a20d09bf08a994
olexxxey <alexeykorneyevets@gmail.com>
2018-10-07 21:51:03 +0300

    i am awesome (not actually)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (property) => {
+    return property;
+}
 
-// return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const createNotEnumerableProperty = (property) => {
+    return Symbol(property);
+}
+
+const createProtoMagicObject = () => {
+    let obj = new Function;
+    obj.__proto__ = obj.prototype;
+    return obj;
+};
+
+let value = 0;
+const incrementor = () => {
+    value += 1;
+    return incrementor;
+};
+incrementor.toString = () => value;
+
+const asyncIncrementor = () => {
+    return new Promise((resolve) => resolve(asyncIncrementor.value += 1));
+}
+asyncIncrementor.value = 1;
+
+const createIncrementer = function () {};
+
+const returnBackInSecond = (param) =>  new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(param);
+    }, 1000);
+});
+
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+
+const createSerializedObject = () => null;
+
+const sortByProto = (array) =>  {
+    return array.sort((a, b) => a.__proto__ == b ? 1 : 0);
+}
+
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
